da14f3481c0f30713ca3bad3ad314ab645dddcb5
“ST”10143151” <ST10143151@vcconnect.edu.za>
2024-10-01 21:44:29 +0200

    Branch Changes

--- CLDV6212_MVCWebApp/Models/CartItem.cs
+++
@@ -1,9 +0,0 @@
-public class CartItem
-{
-    public string ProductId { get; set; }
-    public string ProductName { get; set; }
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
-
-    public decimal TotalPrice => Price * Quantity;
-}
